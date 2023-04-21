57360be952b887a9240ce18ad0abacd93ceb8aa0
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-04-21 10:29:36 +0200

    yes

--- sablecc/grammar/postfix/semantics/IdAttributes.java
+++ sablecc/grammar/postfix/semantics/IdAttributes.java
@@ -1,5 +1,7 @@
 package postfix.semantics;
 
+import java.util.function.Function;
+
 import postfix.node.TId;
 import postfix.node.TType;
 
@@ -14,14 +16,34 @@ public class IdAttributes {
     private String value;
     private boolean isFunction;
     private boolean isConst;
+
+    private Attributes attributes;
+
+    /**Represents information about what modifiers the identifer has */
+    public enum Attributes {
+        /**An identifier whose value cannot be changed */
+        constant,
+        /**An identifier, that is a function */
+        function,
+        //TODO csv skal måske ikke være her
+        /**A special csv type */
+        csv,
+
+    }
     // TODO variabel der indeholder værdi
 
     
     @Deprecated
     public IdAttributes(TId id, TType type, boolean isFunction, boolean isConst) {
     }
-
+    @Deprecated
     public IdAttributes(TId id, TType type, String value, boolean isFunction, boolean isConst) {
+    }
+    public IdAttributes(TId id, TType type, String value, Attributes attributes) {
+        this.id = id;
+        this.type = type;
+        this.value = value;
+        this.attributes = attributes;
     }
     
 
