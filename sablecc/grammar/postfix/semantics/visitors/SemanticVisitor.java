6a931fa555dcb3c4b798282e3605a6315051a0d2
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-05-05 10:57:25 +0200

    jessir

--- sablecc/grammar/postfix/semantics/visitors/SemanticVisitor.java
+++ sablecc/grammar/postfix/semantics/visitors/SemanticVisitor.java
@@ -30,12 +30,38 @@ public class SemanticVisitor extends DepthFirstAdapter {
 
     @Override
     public void inAImportWithoutSeperatorStmt(AImportWithoutSeperatorStmt node) {
+        // ? Måske skal path string være expr i stedet, med type check for at godkende
+        // at det er en string
         // add to symbol table
     }
 
     @Override
     public void inAImportWithSeperatorStmt(AImportWithSeperatorStmt node) {
+        // ? Måske skal path string være expr i stedet, med type check for at godkende
+        // at det er en string
         // add to symbol table
+    }
+
+    @Override
+    public void inAExportStatementStmt(AExportStatementStmt node) {
+        // ? i guess id skal være af typen csv
+        node.getId().apply(new TypeVisitor(symbolTable, "csv"));
+        node.getExpr().apply(new TypeVisitor(symbolTable, "string"));
+    }
+
+    @Override
+    public void inAPrintStatementStmt(APrintStatementStmt node) {
+        // ? skal man kunne printe andet end strings?
+    }
+
+    @Override
+    public void inABreakStatementStmt(ABreakStatementStmt node) {
+        // TODO skal være i break-able block
+    }
+
+    @Override
+    public void inAReturnStmt(AReturnStmt node) {
+        // TODO skal være i funktionsblock
     }
 
     @Override
@@ -43,6 +69,21 @@ public class SemanticVisitor extends DepthFirstAdapter {
         TopDclVisitor dclVisitor = new TopDclVisitor(symbolTable);
         node.apply(dclVisitor);
 
+    }
+
+    @Override
+    public void inAElseBlockStatementElseStatement(AElseBlockStatementElseStatement node) {
+        // TODO ny undersymboltabel
+    }
+
+    @Override
+    public void outAElseBlockStatementElseStatement(AElseBlockStatementElseStatement node) {
+        // TODO tilbage til ydre tabel
+    }
+    @Override
+    public void inAIndexingIndexing(AIndexingIndexing node) {
+        //indeksering med andet end heltal er fy fy
+        node.getExpr().apply(new TypeVisitor(symbolTable, "int"));
     }
 
     @Override
@@ -82,6 +123,17 @@ public class SemanticVisitor extends DepthFirstAdapter {
     }
 
     @Override
+    public void inAWhileLoopStmt(AWhileLoopStmt node) {
+        // the condition expression must be boolean
+        node.getExpr().apply(new TypeVisitor(symbolTable, "bool"));
+    }
+
+    @Override
+    public void inAControlStatementStmt(AControlStatementStmt node) {
+        node.getExpr().apply(new TypeVisitor(symbolTable, "bool"));
+    }
+
+    @Override
     public void inAFunctionCallFunctionCall(AFunctionCallFunctionCall node) {
         // funktionsparametre
 
@@ -94,7 +146,8 @@ public class SemanticVisitor extends DepthFirstAdapter {
     public void inAFunctionCallParamFunctionCallParam(AFunctionCallParamFunctionCallParam node) {
         // TODO antal af givne parametre skal stemme overens med den erklærede funktion
         if (functionParameterTypeList.isEmpty()) {
-            throw new invalidFunctionCallException("Cannot pass parameters to a function that does not take any parameters");
+            throw new invalidFunctionCallException(
+                    "Cannot pass parameters to a function that does not take any parameters");
         }
         node.getExpr().apply(new TypeVisitor(symbolTable, functionParameterTypeList.remove()));
 
@@ -109,8 +162,10 @@ public class SemanticVisitor extends DepthFirstAdapter {
                 i++;
                 parent = parent.parent();
             }
-            AFunctionCallFunctionCall functionCallNode = (AFunctionCallFunctionCall)parent;
-            throw new invalidFunctionCallException("Cannot pass " +i + " parameters to a function that only takes " +symbolTable.get(functionCallNode.getId().getText()).getParameterTypeListAsQueueList().size()+ " parameters");
+            AFunctionCallFunctionCall functionCallNode = (AFunctionCallFunctionCall) parent;
+            throw new invalidFunctionCallException("Cannot pass " + i + " parameters to a function that only takes "
+                    + symbolTable.get(functionCallNode.getId().getText()).getParameterTypeListAsQueueList().size()
+                    + " parameters");
         }
         node.getExpr().apply(new TypeVisitor(symbolTable, functionParameterTypeList.remove()));
     }
@@ -118,7 +173,8 @@ public class SemanticVisitor extends DepthFirstAdapter {
     @Override
     public void outAFunctionCallFunctionCall(AFunctionCallFunctionCall node) {
         if (!functionParameterTypeList.isEmpty()) {
-            throw new invalidFunctionCallException("Function" +node.getId().getText()+"()" + " takes "+ functionParameterTypeList.size() +" more parameters"+ " at line " + node.getId().getLine());
+            throw new invalidFunctionCallException("Function" + node.getId().getText() + "()" + " takes "
+                    + functionParameterTypeList.size() + " more parameters" + " at line " + node.getId().getLine());
         }
         // symbolTable = symbolTable.getOuterSymbolTable();
     }
