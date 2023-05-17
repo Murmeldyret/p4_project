4a34a3de0af4cb14d1826ef412472d1bb094ee03
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-05-17 10:09:40 +0200

    tests if function paramter declarations are a valid type

---
+++ sablecc/grammar/postfix/semantics/Exceptions/InvalidDeclarationException.java
@@ -0,0 +1,15 @@
+package postfix.semantics.Exceptions;
+
+import postfix.node.PDcl;
+
+public class InvalidDeclarationException extends TypeException {
+
+    private PDcl node;
+    public InvalidDeclarationException(String message) {
+        super(message);
+    }
+    public InvalidDeclarationException(String message, PDcl node) {
+        super(message);
+        this.node = node;
+    }
+}
