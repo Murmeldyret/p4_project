79711a7510fc148b06c0a25eb921076ceb51964d
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2023-05-21 13:48:36 +0200

    New csv visitor :)

---
+++ sablecc/grammar/postfix/semantics/visitors/CodeGen/CsvVisitorCodeGen.java
@@ -0,0 +1,14 @@
+package postfix.semantics.visitors.CodeGen;
+
+import postfix.analysis.DepthFirstAdapter;
+import postfix.node.AAddToCsvCsvOp;
+import postfix.node.AImportWithoutSeperatorStmt;
+
+public class CsvVisitorCodeGen extends DepthFirstAdapter{
+    String csvOperations;
+
+    @Override
+    public void inAImportWithoutSeperatorStmt(AImportWithoutSeperatorStmt node) {
+        csvOperations = "Csvruntime " + node.getId().toString() + "= new Csvruntime(" node.getString().getText().toString() + ");";
+    }
+}
