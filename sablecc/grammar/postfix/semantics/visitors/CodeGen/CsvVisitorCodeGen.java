dc793e4291157b1197b23400a9332c6895af8eb7
soft-MRJ <mikeriberj@gmail.com>
2023-05-22 14:56:01 +0200

    Added the special operations for csv

--- sablecc/grammar/postfix/semantics/visitors/CodeGen/CsvVisitorCodeGen.java
+++ sablecc/grammar/postfix/semantics/visitors/CodeGen/CsvVisitorCodeGen.java
@@ -2,11 +2,18 @@ package postfix.semantics.visitors.CodeGen;
 
 import postfix.analysis.DepthFirstAdapter;
 import postfix.node.AAddToCsvCsvOp;
+import postfix.node.ACountSpecialSyntax;
 import postfix.node.ACsvOperationStmt;
+import postfix.node.AFilterSpecialSyntax;
 import postfix.node.AImportWithoutSeperatorStmt;
 import postfix.node.AInsertFromCsvCsvOp;
+import postfix.node.AMeanSpecialSyntax;
 import postfix.node.ARemoveAtFromCsvCsvOp;
 import postfix.node.ARemoveFromCsvCsvOp;
+import postfix.node.ASortAscSpecialSyntax;
+import postfix.node.ASortDescSpecialSyntax;
+import postfix.node.ASortSpecialSyntax;
+import postfix.node.ASumSpecialSyntax;
 
 public class CsvVisitorCodeGen extends DepthFirstAdapter{
     String csvOperations;
@@ -54,5 +61,40 @@ public class CsvVisitorCodeGen extends DepthFirstAdapter{
         } else if (node.getOrientation().toString() == "column") {
             csvOperations = node.getId().getText() + "." + "insertColumn(" + node.getVal().toString() + ", " + node.getExpr().toString() + ", " + node.getArrayExpr().toString() + ")";
         }
+    }
+
+    @Override
+    public void inASumSpecialSyntax(ASumSpecialSyntax node) {
+        csvOperations = ".sum(" + node.getExpr().toString() + ")";
+    }
+
+    @Override
+    public void inAFilterSpecialSyntax(AFilterSpecialSyntax node) {
+        csvOperations = ".filter(" + node.getExpr().toString() + "," + node.getFilterexpr().toString() + ")";
+    }
+
+    @Override
+    public void inACountSpecialSyntax(ACountSpecialSyntax node) {
+        csvOperations = ".count()";
+    }
+
+    @Override
+    public void inAMeanSpecialSyntax(AMeanSpecialSyntax node) {
+        csvOperations = ".mean(" + node.getExpr().toString() + ")";
+    }
+
+    @Override
+    public void inASortSpecialSyntax(ASortSpecialSyntax node) {
+        csvOperations = ".sort(" + node.getExpr().toString() + ", false)";
+    }
+
+    @Override
+    public void inASortDescSpecialSyntax(ASortDescSpecialSyntax node) {
+        csvOperations = ".sort(" + node.getExpr().toString() + ", true)";
+    }
+
+    @Override
+    public void inASortAscSpecialSyntax(ASortAscSpecialSyntax node) {
+        csvOperations = ".sort(" + node.getExpr().toString() + ", false)";
     }
 }
