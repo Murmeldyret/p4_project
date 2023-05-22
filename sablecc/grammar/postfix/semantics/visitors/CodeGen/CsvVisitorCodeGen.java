3afddfe02a8fa908b9b05d712a81eda0a08642cc
soft-MRJ <mikeriberj@gmail.com>
2023-05-22 14:38:05 +0200

    Added csv operations Changed the sableCC grammar for csv "add" and "insert"

--- sablecc/grammar/postfix/semantics/visitors/CodeGen/CsvVisitorCodeGen.java
+++ sablecc/grammar/postfix/semantics/visitors/CodeGen/CsvVisitorCodeGen.java
@@ -2,13 +2,57 @@ package postfix.semantics.visitors.CodeGen;
 
 import postfix.analysis.DepthFirstAdapter;
 import postfix.node.AAddToCsvCsvOp;
+import postfix.node.ACsvOperationStmt;
 import postfix.node.AImportWithoutSeperatorStmt;
+import postfix.node.AInsertFromCsvCsvOp;
+import postfix.node.ARemoveAtFromCsvCsvOp;
+import postfix.node.ARemoveFromCsvCsvOp;
 
 public class CsvVisitorCodeGen extends DepthFirstAdapter{
     String csvOperations;
 
     @Override
     public void inAImportWithoutSeperatorStmt(AImportWithoutSeperatorStmt node) {
-        csvOperations = "Csvruntime " + node.getId().toString() + "= new Csvruntime(" + node.getString().getText().toString() + ");";
+        csvOperations = "Csvruntime " + node.getId().toString() + "= new Csvruntime(" + node.getExpr().toString() + ");";
+    }
+
+    @Override
+    public void inAAddToCsvCsvOp(AAddToCsvCsvOp node) {
+        if (node.getOrientation().toString() == "row") {
+            csvOperations = node.getId().getText() + "." + "addRow(" + node.getArrayExpr().toString() + ")";
+
+        } else if (node.getOrientation().toString() == "column") {
+            csvOperations = node.getId().getText() + "." + "addColumn(" + node.getExpr().toString() + ", " + node.getArrayExpr().toString() + ")";
+        }
+    }
+
+    @Override
+    public void inARemoveFromCsvCsvOp(ARemoveFromCsvCsvOp node) {
+        if (node.getOrientation().toString() == "row") {
+            csvOperations = node.getId().getText() + "." + "removeRow()";
+
+        } else if (node.getOrientation().toString() == "column") {
+            csvOperations = node.getId().getText() + "." + "removeColumn()";
+        }
+    }
+
+    @Override
+    public void inARemoveAtFromCsvCsvOp(ARemoveAtFromCsvCsvOp node) {
+        if (node.getOrientation().toString() == "row") {
+            csvOperations = node.getId().getText() + "." + "removeRow(" + node.getVal().toString() + ")";
+
+        } else if (node.getOrientation().toString() == "column") {
+            csvOperations = node.getId().getText() + "." + "removeColumn(" + node.getVal().toString() + ")";
+        }
+    }
+
+    @Override
+    public void inAInsertFromCsvCsvOp(AInsertFromCsvCsvOp node) {
+        if (node.getOrientation().toString() == "row") {
+            csvOperations = node.getId().getText() + "." + "insertRow(" + node.getVal().toString() + ", " + node.getArrayExpr().toString() + ")";
+
+        } else if (node.getOrientation().toString() == "column") {
+            csvOperations = node.getId().getText() + "." + "insertColumn(" + node.getVal().toString() + ", " + node.getExpr().toString() + ", " + node.getArrayExpr().toString() + ")";
+        }
     }
 }
